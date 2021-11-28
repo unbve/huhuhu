53c55e3afb8ee678a45387e2c5e63b05211c5ec5
unbve <95178005+unbve@users.noreply.github.com>
2021-11-28 23:18:01 +0800

    Add files via upload

---
+++ Amenities.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="design.css">
+    <title>Amenities</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-main">
+    <!--ref="#">
+            <img src="burnay.png" alt="" width="70" height="70" class="d-inline-block">
+            Burnay Sands
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="nav navbar-nav ms-auto p-2">
+              <li class="nav-item">
+                <a class="nav-link" href="Intro.html">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Reviews.html">REVIEWS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="Amenities.html">AMENITIES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Reservation.html">RESERVATION</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="ContactUs.html">CONTACT US</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">LOG-IN</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>-->
+      <div class="container">
+          <h1 class="Ame">AMENITIES</h1>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="OutsideOfTheBeach.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Outside of the Beach House</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="TakeALook.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Take a look inside of the Beach House</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="CampingTents.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Camping Tents</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="RetractableBeds.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Retractable Beds</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="BeachCottages.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Beach Cottages</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="PicnicHuts.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Picnic Huts</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="Big&Small.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Big and Small Motor boats for Touring and Fishing</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="Kayaks&Snorkling.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Kayaks and Snorkling</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="NightTimePool.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Night time at the pool and gazebo</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel carousel_dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="CaterWedding.png" class="amenities_pic d-inline-block w-75" alt="secret">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="amenities_title">Caters Weddings and other Events</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"
+    integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG"
+    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"
+    integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc"
+    crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ ContactUs.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="design.css">
+    <title>Contact Us</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-main">
+    <!--<nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top" style="background-color: rgba(239, 248, 247, 0.76);">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="burnay.png" alt="" width="70" height="70" class="d-inline-block">
+            Burnay Sands
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="nav navbar-nav ms-auto p-2">
+              <li class="nav-item">
+                <a class="nav-link" href="Intro.html">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Reviews.html">REVIEWS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Amenities.html">AMENITIES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Reservation.html">RESERVATION</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="ContactUs.html">CONTACT US</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">LOG-IN</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>-->
+      <div class="container">
+          <h1 class="Rev">CONTACT US</h1>
+      </div>
+      <div class="green_container container">
+          <div class="row">
+            <div class="col-md-8">
+              <div class="well well-sm">
+                <form>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="number">0917 717 1166</label>
+                      </div>
+                      <div class="form-group">
+                        <label for="barangay">Barangay Poblacion, Gitagum, Misamis Oriental</label>
+                      </div>
+                      <div class="form-group">
+                        <label for="email">burnaysands@yahoo.com</label>
+                      </div>
+                    </div>
+                    <div class="class responsive col-md-4">
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75067.6802005898!2d124.36999020664123!3d8.571081438843954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32555d98cf750a37%3A0xfd2d7c19e47fafaa!2sBURNAY%20SANDS!5e0!3m2!1sen!2sph!4v1636251045912!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"
+    integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG"
+    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"
+    integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc"
+    crossorigin="anonymous"></script>
+</body>
+</html>
